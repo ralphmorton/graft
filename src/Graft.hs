386311132490ca86a28001d14f3badb143b7f561
Ralph Morton <ralphmorton@gmail.com>
2016-06-10 00:41:59 +0200

    Support for basic single-level nesting

--- src/Graft.hs
+++ src/Graft.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Graft(
     module Graft.Types,
@@ -19,14 +21,63 @@ import qualified Data.Text as T
 import System.Directory (doesFileExist, getDirectoryContents)
 import Text.Parsec
 import Text.Parsec.String
+import Debug.Trace
 
--- |Attach a new template
-attachTemplate :: GraftData -> String -> String -> Either String GraftData
-attachTemplate (GraftData tpls) tpln tpl = GraftData . flip (M.insert tpln) tpls <$> pres
+data Contain a = forall a. TemplateData a => C a
+
+instance TemplateData (Contain a) where
+    child k (C a) = child k a
+
+-- |Compile a template
+graft :: (MonadError e m, AsGraftError e, MonadReader r m, HasGraftData r, TemplateData a) => String -> a -> m T.Text
+graft name vars = do
+    (GraftData tpls) <- view graftData
+    maybe (throwG GraftMissingTemplate) (graftTpl vars) (M.lookup name tpls)
+
+graftTpl :: (MonadError e m, AsGraftError e, TemplateData a) => a -> Template -> m T.Text
+graftTpl vars tpl = do
+    let ox = compile vars <$> tpl
+    let res = foldM (\s -> either Left (Right . mappend s)) mempty ox
+    case res of
+        Left e -> throwG e
+        Right r -> return r
+
+compile :: TemplateData a => a -> Chunk -> Either GraftError T.Text
+compile _ (Lit t) = Right t
+compile vm (Bind key) = case (child k vm) of
+    Nothing -> trace (show k) $ Left $ GraftMissingVariable key
+    Just v -> case (resolve v kx) of
+        Left e -> Left e
+        Right (Val val) -> Right val
+        Right _ -> Left $ GraftVariableMismatch key
     where
-    pres = case (parse parseTemplate "" tpl) of
-        Left e -> Left $ show e
-        Right r -> Right r
+    (k:kx) = T.splitOn "." key
+compile vm (Loop bound key tpl) = case (child k vm) of
+    Nothing -> Left $ GraftMissingVariable key
+    Just v -> case (resolve v kx) of
+        Left e -> Left e
+        Right (Array vx) -> do
+            tx <- mapM (compileLoop bound tpl vm) vx
+            return $ mconcat tx
+        Right _ -> Left $ GraftVariableMismatch key
+    where
+    (k:kx) = T.splitOn "." key
+
+compileLoop :: (TemplateData a, TemplateData b) => T.Text -> Template -> a -> b -> Either GraftError T.Text
+compileLoop bound tpl a b = do
+    let bmap = M.fromList [(bound, Object b)]
+    let dta = [C bmap, C a]
+    graftTpl dta tpl
+
+resolve :: Var -> [T.Text] -> Either GraftError Var
+resolve v [] = Right v
+resolve (Object o) (k:kx) = case (child k o) of
+    Nothing -> Left $ GraftMissingVariable k
+    Just v -> resolve v kx
+resolve _ (k:_) = Left $ GraftVariableMismatch k
+
+throwG :: (MonadError e m, AsGraftError e) => GraftError -> m a
+throwG = throwError . review _GraftError
 
 -- |Load all templates from a directory
 loadDirectory :: MonadIO m => String -> m (Either String GraftData)
@@ -43,42 +94,66 @@ loadTpl path = liftIO $ do
     dta <- readFile path
     return (fname, dta)
 
-graft :: (MonadError e m, AsGraftError e, MonadReader r m, HasGraftData r) => String -> VarMap -> m T.Text
-graft name vars = do
-    (GraftData tpls) <- view graftData
-    flip (maybe (throwG GraftMissingTemplate)) (M.lookup name tpls) $ \tpl -> do
-        let ox = compile vars <$> tpl
-        let res = foldM (\s -> either Left (Right . mappend s)) mempty ox
-        case res of
-            Left e -> throwG e
-            Right r -> return r
-
-compile :: VarMap -> Chunk -> Either GraftError T.Text
-compile _ (Lit t) = Right t
-compile vm (Bind k) = case (M.lookup k vm) of
-    Nothing -> Left $ GraftMissingVariable (T.unpack k)
-    Just val -> Right val
-
-throwG :: (MonadError e m, AsGraftError e) => GraftError -> m a
-throwG = throwError . review _GraftError
+-- |Attach a new template
+attachTemplate :: GraftData -> String -> String -> Either String GraftData
+attachTemplate (GraftData tpls) tpln tpl = GraftData . flip (M.insert tpln) tpls <$> pres
+    where
+    pres = case (parse parseTemplate "" tpl) of
+        Left e -> Left $ show e
+        Right r -> Right r
 
 parseTemplate :: Parser Template
-parseTemplate = many1 (lit <|> mvar)
+parseTemplate = many1 (lit <|> mbind <|> mloop)
 
 lit :: Parser Chunk
-lit = Lit . T.pack <$> many1 (noneOf "{")
+lit = Lit . T.pack <$> many1 (noneOf "{[")
 
-mvar :: Parser Chunk
-mvar = (try var) <|> rchunk
+mbind :: Parser Chunk
+mbind = (try bind) <|> rchunk
     where
     rchunk = do
         char '{'
-        rv <- many $ noneOf "{"
+        rv <- many $ noneOf "{["
         return . Lit . T.pack $ ("{" ++ rv)
 
-var :: Parser Chunk
-var = do
+bind :: Parser Chunk
+bind = do
     string "{{"
     v <- T.strip . T.pack <$> many1 (noneOf "}")
     string "}}"
     return (Bind v)
+
+mloop :: Parser Chunk
+mloop = (try loop) <|> rchunk
+    where
+    rchunk = do
+        char '['
+        rv <- many $ noneOf "{["
+        return . Lit . T.pack $ ("[" ++ rv)
+
+loop :: Parser Chunk
+loop = do
+    (b, n) <- loopStart
+    dta <- manyTill anyChar loopEnd
+    case (parse parseTemplate "" dta) of
+        Left e -> fail (show e)
+        Right tpl -> return (Loop b n tpl)
+
+
+loopStart :: Parser (T.Text, T.Text)
+loopStart = do
+    string "[["
+    b <- T.strip . T.pack <$> many1 (noneOf ":")
+    char ':'
+    n <- T.strip . T.pack <$> many1 (noneOf "]")
+    string "]]"
+    return (b, n)
+
+loopEnd :: Parser ()
+loopEnd = do
+    string "[["
+    many (char ' ')
+    string "end"
+    many (char ' ')
+    string "]]"
+    return ()
--- src/Graft/Types.hs
+++ src/Graft/Types.hs
@@ -1,34 +1,54 @@
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 
 module Graft.Types where
 
+import Control.Applicative ((<|>))
 import Control.Lens (Lens')
 import Control.Lens.TH
 import qualified Data.Map as M
 import Data.Monoid
 import Data.Text (Text)
 
+class TemplateData a where
+    child :: Text -> a -> Maybe Var
+
 data GraftData = GraftData {
     _graftDataTemplates :: M.Map String Template
 } deriving Show
 
 type Template = [Chunk]
-type VarMap = M.Map Text Text
+type VarMap = M.Map Text Var
 
 data Chunk
     = Lit Text
     | Bind Text
+    | Loop Text Text Template --name bound_as subtemplate
     deriving Show
+
+data Var where
+    Val :: Text -> Var
+    Object :: TemplateData a => a -> Var
+    Array :: TemplateData a => [a] -> Var
 
 data GraftError
     = GraftMissingTemplate
-    | GraftMissingVariable String
+    | GraftMissingVariable Text
+    | GraftVariableMismatch Text
     deriving Show
 
 instance Monoid GraftData where
     mempty = GraftData M.empty
     mappend (GraftData ta) (GraftData tb) = GraftData $ M.union ta tb
+
+instance TemplateData a => TemplateData [a] where
+    child _ [] = Nothing
+    child k (d:dx) = child k d <|> child k dx
+
+instance TemplateData VarMap where
+    child = M.lookup
 
 makeClassy ''GraftData
 makeClassyPrisms ''GraftError
